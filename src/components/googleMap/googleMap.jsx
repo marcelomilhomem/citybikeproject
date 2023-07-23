e1610bc0983801156c74b2c6a1c5f987c01b7df2
Marcelo Milhomem <marcelomilhomemm@hotmail.com>
2023-07-23 12:44:45 +0100

    Clusters working

--- src/components/googleMap/googleMap.jsx
+++ src/components/googleMap/googleMap.jsx
@@ -4,27 +4,10 @@ import {
   InfoWindowF,
   MarkerF,
   useLoadScript,
+  MarkerClusterer,
 } from "@react-google-maps/api";
 import axios from "axios";
 import { Center, Heading } from "@chakra-ui/react";
-
-const markers = [
-  {
-    id: 1,
-    name: "Qobustan",
-    position: { lat: 40.0709493, lng: 49.3694411 },
-  },
-  {
-    id: 2,
-    name: "Sumqayit",
-    position: { lat: 40.5788843, lng: 49.5485073 },
-  },
-  {
-    id: 3,
-    name: "Baku",
-    position: { lat: 40.3947365, lng: 49.6898045 },
-  }
-];
 
 function Map() {
   const { isLoaded } = useLoadScript({
@@ -36,14 +19,14 @@ function Map() {
   const [activeMarkerId, setActiveMarkerId] = useState(null);
   const [networks, setNetworks] = useState(null);
   const [showNetworks, setShowNetworks] = useState(null);
-  const [center, setCenter] = useState({ lat: 38.699708, lng: -9.439973 })
+  const [center, setCenter] = useState({ lat: 38.699708, lng: -9.439973 });
 
   const handleActiveMarker = (markerId, latitude, longitude) => {
     if (markerId === activeMarkerId) {
       return;
     }
     setActiveMarkerId(markerId);
-    setCenter({lat: latitude, lng: longitude})
+    setCenter({ lat: latitude, lng: longitude });
   };
 
   useEffect(() => {
@@ -51,7 +34,7 @@ function Map() {
       const response = await axios.get(`${apiURL}`);
       const networkData = response.data.networks;
       setNetworks(networkData);
-      setShowNetworks(true)
+      setShowNetworks(true);
     };
 
     fetchNetworks();
@@ -60,9 +43,6 @@ function Map() {
   return (
     <Fragment>
       {console.log(networks)}
-      <div className="container">
-        <h1 className="text-center">Vite + React | Google Map Markers</h1>
-        <div style={{ height: "90vh", width: "100%" }}>
           {isLoaded ? (
             <GoogleMap
               center={center}
@@ -70,32 +50,43 @@ function Map() {
               onClick={() => setActiveMarkerId(null)}
               mapContainerStyle={{ width: "100%", height: "90vh" }}
             >
-              {showNetworks && networks.map((network) => (
-                <MarkerF
-                  key={network.id}
-                  position={{
-                    lat: network.location.latitude,
-                    lng: network.location.longitude
-                  }}
-                  onClick={() => handleActiveMarker(network.id, network.location.latitude, network.location.longitude)}
-                  // icon={{
-                  //   url:"https://t4.ftcdn.net/jpg/02/85/33/21/360_F_285332150_qyJdRevcRDaqVluZrUp8ee4H2KezU9CA.jpg",
-                  //   scaledSize: { width: 50, height: 50 }
-                  // }}
-                >
-                  {activeMarkerId === network.id ? (
-                    <InfoWindowF onCloseClick={() => setActiveMarkerId(null)}>
-                      <Center>
-                        <Heading size={'md'} color={'black'}>{network.location.city}</Heading>
-                      </Center>
-                    </InfoWindowF>
-                  ) : null}
-                </MarkerF>
-              ))}
+              {showNetworks && (
+                <MarkerClusterer averageCenter enableRetinaIcons gridSize={60}>
+                  {(clusterer) =>
+                    networks.map((network) => (
+                      <MarkerF
+                        key={network.id}
+                        position={{
+                          lat: network.location.latitude,
+                          lng: network.location.longitude,
+                        }}
+                        onClick={() =>
+                          handleActiveMarker(
+                            network.id,
+                            network.location.latitude,
+                            network.location.longitude,
+                          )
+                        }
+                        clusterer={clusterer}
+                      >
+                        {activeMarkerId === network.id ? (
+                          <InfoWindowF
+                            onCloseClick={() => setActiveMarkerId(null)}
+                          >
+                            <Center>
+                              <Heading size={"md"} color={"black"}>
+                                {network.location.city}
+                              </Heading>
+                            </Center>
+                          </InfoWindowF>
+                        ) : null}
+                      </MarkerF>
+                    ))
+                  }
+                </MarkerClusterer>
+              )}
             </GoogleMap>
           ) : null}
-        </div>
-      </div>
     </Fragment>
   );
 }
